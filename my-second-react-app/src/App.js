b968cb594ddc8098e0f6eac3218392e79577ceb6
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-05 21:46:09 -0600

    push working on components

--- my-second-react-app/src/App.js
+++ my-second-react-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { useState } from 'react'
+import Title from './components/Title';
+
+
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true)
+  
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "browser's live stream", id: 2},
+    {title: "race on Moo moo Farm", id: 3}
+  ])
+
+  const handleClick = (id)=> {
+    setEvents((prevEvents)=>{
+      return prevEvents.filter((event)=>{
+        return id !== event.id
+      })
+    })
+   console.log(id)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      { showEvents && (<div>
+        <button onClick={()=>setShowEvents(false)}>Hide Events</button>
+      </div>)}
+      {!showEvents && (<div>
+        <button onClick={()=> setShowEvents(true)}>Show Events</button>
+      </div>)}
+     
+      {showEvents && events.map((event)=> (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+          <button onClick={()=> handleClick(event.id)}>Delete event</button>
+        </div>
+      ))}
+      
     </div>
   );
 }
---
+++ my-second-react-app/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export default function Title() {
+  return(
+    <div>
+      <h1 className="title">Mario's Kingdom Events</h1>
+      <br />
+      <h2 className="subtitle"> All the latest events in Mario's Kingdom</h2>
+    </div>
+  )
+}

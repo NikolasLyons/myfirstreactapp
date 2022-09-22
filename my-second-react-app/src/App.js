7f6f1f3da8df386af35fd5b6ba80555870c3675b
Nik Lyons <nikolas.lyons23@gmail.com>
2022-09-21 22:20:40 -0600

    working on events

--- my-second-react-app/src/App.js
+++ my-second-react-app/src/App.js
@@ -5,56 +5,59 @@ import { useState } from 'react'
 import Title from './components/Title';
 import Modal from './components/Modal';
 import EventList from './components/EventList';
+import NewEventForm from './components/NewEventForm';
 
 
 
 function App() {
   const [showModal, setShowModal] = useState(false)
   const [showEvents, setShowEvents] = useState(true)
-  
-  const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "browser's live stream", id: 2},
-    {title: "race on Moo moo Farm", id: 3}
-  ])
 
-  const handleClick = (id)=> {
-    setEvents((prevEvents)=>{
-      return prevEvents.filter((event)=>{
+  const [events, setEvents] = useState([])
+
+  const addEvent = (event) => {
+    setEvents((prevEvents) => {
+      return [...prevEvents, event]
+    })
+
+  }
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
         return id !== event.id
       })
     })
-   console.log(id)
+    console.log(id)
   }
 
-  const handleOpen= () =>{
+  const handleOpen = () => {
     setShowModal(true)
   }
 
-  const handleClose = () =>{
+  const handleClose = () => {
     setShowModal(false)
-   
+
   }
   return (
     <div className="App">
-      
+
       <Title title="Events in your area" />
 
-      { showEvents && (<div>
-        <button onClick={()=>setShowEvents(false)}>Hide Events</button>
+      {showEvents && (<div>
+        <button onClick={() => setShowEvents(false)}>Hide Events</button>
       </div>)}
       {!showEvents && (<div>
-        <button onClick={()=> setShowEvents(true)}>Show Events</button>
+        <button onClick={() => setShowEvents(true)}>Show Events</button>
       </div>)}
-     
+
       {showEvents && <EventList events={events} handleClick={handleClick} />}
-      { showModal && <Modal handleClose={handleClose}>
-      <h2> 10% Off Coupon Code!</h2>
-        <p>Use the code NINJA10 at the check out</p>
+      {showModal && <Modal handleClose={handleClose}>
+        <NewEventForm addEvent={addEvent} />
       </Modal>}
       <div>
 
-      <button onClick={handleOpen}>Show Modal</button>
+        <button onClick={handleOpen}>Add New Event</button>
       </div>
     </div>
   );
--- my-second-react-app/src/components/EventList.js
+++ my-second-react-app/src/components/EventList.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import styles from './EventList.module.css'
 
-export default function EventList({events, handleClick}) {
+
+
+export default function EventList({ events, handleClick }) {
   return (
-    <div>{events.map((event)=> (
-      <React.Fragment key={event.id}>
+    <div>{events.map((event) => (
+      <div className={styles.card} key={event.id}>
         <h2>{event.title}</h2>
-        <button onClick={()=> handleClick(event.id)}>Delete event</button>
-      </React.Fragment>
+        <button onClick={() => handleClick(event.id)}>Delete event</button>
+      </div>
     ))}</div>
   )
 }
